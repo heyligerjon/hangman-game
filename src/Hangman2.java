938671415ce06b01911ae3383245686fd5205944
Jon Isaacs-Heyliger <36351254+heyligerjon@users.noreply.github.com>
2023-02-27 13:50:27 -0500

    Update Hangman2.java

--- src/Hangman2.java
+++ src/Hangman2.java
@@ -1,27 +1,3 @@
-/*
-* Hangman2.java
-* Author: Jon Isaacs-Heyliger
-* Submission Date: 3/18/16
-*
-* Purpose: The purpose of this program is to run a game of hangman 
-* for the user to play
-*
-* Statement of Academic Honesty:
-*
-* The following code represents my own work. I have neither
-* received nor given inappropriate assistance. I have not copied
-* or modified code from any source other than the course webpage
-* or the course textbook. I recognize that any unauthorized
-* assistance or plagiarism will be handled in accordance with
-* the University of Georgia's Academic Honesty Policy and the
-* policies of this course. I recognize that my work is based
-* on an assignment created by the Department of Computer
-* Science at the University of Georgia. Any publishing
-* or posting of source code for this project is strictly
-* prohibited unless you have written consent from the Department
-* of Computer Science at the University of Georgia.
-*/
-
 import java.util.Scanner;
 
 public class Hangman2 {
